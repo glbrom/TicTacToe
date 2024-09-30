6e8103ad760ec6daea8e293e295b38ee318ec59b
SergeyZakurakin <pricezak@gmail.com>
2024-09-30 15:33:17 -0400

    add toolbar

--- TicTacToe/Views/OnboardingView/OnboardingView.swift
+++ TicTacToe/Views/OnboardingView/OnboardingView.swift
@@ -11,21 +11,42 @@ struct OnboardingView: View {
     var body: some View {
         
         NavigationView {
-            VStack(spacing: 40) {
-                Spacer()
-                Image("OnboardingLabel")
-                    .resizable()
-                    .frame(width: 260, height: 135)
+            
+            ZStack {
+                Color.appBackground
+                    .ignoresSafeArea()
                 
-                Text("TIC-TAC-TOE")
-                    .font(.system(size: 38, weight: .medium))
-                
-                Spacer()
-                
-                NavigationLink(destination: SelectGameView()) {
-                    PurpleButtonView(buttonTitle: "Let's Play")
+                VStack(spacing: 30) {
+                    Spacer()
+                    Image("OnboardingLabel")
+                        .resizable()
+                        .frame(width: 260, height: 135)
+                    
+                    Text("TIC-TAC-TOE")
+                        .font(.system(size: 38, weight: .medium))
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: SelectGameView()) {
+                        PurpleButtonView(buttonTitle: "Let's Play")
+                    }
+                    .padding(.vertical, 40)
                 }
             }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    NavigationLink(destination: SelectGameView()) {
+                        Image(systemName: "flame.fill")
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(destination: SelectGameView()) {
+                        Image(systemName: "flame.fill")
+                    }
+                }
+            }
+            
         }
     }
 }
