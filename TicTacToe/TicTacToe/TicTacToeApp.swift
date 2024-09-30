c898623530bdf1878739a42e943da6ec084474ee
glbrom <golbrm@gmail.com>
2024-09-30 17:51:28 +0300

    create App

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Roman Golub on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
