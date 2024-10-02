51ce0e885f0580aa33575095f35df36b752973c2
glbrom <golbrm@gmail.com>
2024-10-02 13:31:58 +0300

    create PlayerView

--- TicTacToe/Views/GameView/Views/PlayerView.swift
+++ TicTacToe/Views/GameView/Views/PlayerView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct PlayerView: View {
+    var playerIcon: String
+    var playerName: String
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            VStack {
+                Image(playerIcon)
+                .frame(width: 54, height: 54)
+                
+                Text(playerName)
+                .font(.system(size: 16, weight: .semibold))
+                .frame(width: 83, height: 20, alignment: .top)
+            }
+               
+        }
+        .frame(width: 100, height: 100)
+        .padding(10)
+        .background(Color.appLightBlue)
+        .cornerRadius(30)
     }
 }
 
 #Preview {
-    PlayerView()
+    PlayerView(playerIcon: "Xskin1", playerName: "You")
 }
